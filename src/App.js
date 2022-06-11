a71898ddb733d0207e7e22480e89f40f68f1e24f
Kyle Frame <kjframe1223@gmail.com>
2022-06-10 22:42:33 -0500

    added lord stanley

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import LordStanley from './app/assets/img/Lord-Stanley.png';
 
 function App() {
   return (
     <div className="App">
-      hello hockey fans!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={LordStanley} alt='lordstanley logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for hockey!
     </div>
   );
 }
