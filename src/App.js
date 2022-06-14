15cc2d5625d625e74c137b56249d34281b65ecd7
Kyle Frame <kjframe1223@gmail.com>
2022-06-13 21:22:12 -0500

    router set up

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import React from 'react'
 import background from './app/assets/img/ice.jpg'
-
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div style={{ height: '100vh', width: '100vw', backgroundSize: 'cover', backgroundImage: `url(${background})` }} className="App">
       <Header />
-      <TeamsDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />          
+        <Route path='directory' element={<TeamsDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/features/teams/TeamCard.js
+++ src/features/teams/TeamCard.js
@@ -1,15 +1,13 @@
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText } from 'reactstrap';
 
 const TeamCard = ({ team }) => {
     const { image, name } = team;
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '20rem' }}>
             <CardImg  width='100%' src={image} alt={name} variant="top" />
             <CardBody>
-                <CardTitle>Card Title</CardTitle>
                 <CardText>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
                 </CardText>
             </CardBody>
         </Card>
