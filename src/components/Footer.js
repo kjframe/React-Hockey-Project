801f8b35107264e23a8dcf53cb55ecc2d53ebe70
Kyle Frame <kjframe1223@gmail.com>
2022-06-13 21:40:15 -0500

    Header/Footer Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 const Footer = () => {
@@ -6,11 +7,25 @@ const Footer = () => {
         <footer className='site-footer'>
             <Container>
                 <Row>
-                    <Col xs={{ size: 4, offset: 1,}} sm='2'>
-                    <h5 style={{ color: "#1DA1F2" }}>this will be site navigation!</h5>
+                    <Col xs={{ size: 4, offset: 1 }} sm='2'>
+                        <h5 style={{ color: "#1DA1F2" }}>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
                     </Col>
                     <Col xs='6' sm='3' className='text-center'>
-                    <h5 style={{ color: "#1DA1F2" }}>Social</h5>
+                        <h5 style={{ color: "#1DA1F2" }}>Social</h5>
                         <a
                             className='btn btn-social-icon btn-instagram'
                             href='http://instagram.com/'
@@ -50,7 +65,7 @@ const Footer = () => {
                             className='btn btn-link'
                             href='mailto:notreal@notreal.co'
                         >
-                            <i className='fa fa-envelope-o' /> midsouth_hockey@memphis.com
+                            <i className='fa fa-envelope-o' /> memphis_hockey@memphis.com
                         </a>
                     </Col>
                 </Row>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,49 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { useState } from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav,
+    NavItem,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import LordStanley from '../app/assets/img/Lord-Stanley.png'
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <NavbarBrand href='/'>
-                <img src={LordStanley} alt='lordstanley logo' />
+        <Navbar dark color='black' sticky='top' expand='md'>
+            <NavbarBrand className='ms-5' href='/'>
+                <img src={LordStanley} alt='lordstanley logo' className='float-start' />
+                <h1 className='mt-4'>Hockey</h1>
             </NavbarBrand>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink style={{ color: "#1DA1F2" }} className='nav-link' to='/'>
+                            <i style={{ color: "#1DA1F2" }} className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink style={{ color: "#1DA1F2" }} className='nav-link' to='/directory'>
+                            <i style={{ color: "#1DA1F2" }} className='fa fa-list fa-lg' /> Directory
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink style={{ color: "#1DA1F2" }} className='nav-link' to='/about'>
+                            <i style={{ color: "#1DA1F2" }} className='fa fa-info fa-lg' /> About
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink style={{ color: "#1DA1F2" }} className='nav-link' to='/contact'>
+                            <i style={{ color: "#1DA1F2" }} className='fa fa-address-card fa-lg' /> Contact
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
         </Navbar>
     );
 }
